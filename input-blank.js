c69c45aacb75f8bb7fbb2d4ec2d0cb5cff6c3fbb
m1ke <git@m1ke.me>
2016-11-30 12:40:13 +0000

    Update input blank to increment counts on fields

--- input-blank.js
+++ input-blank.js
@@ -1,7 +1,16 @@
 ;(function($){
 	$.fn.inputBlankArray=function(){
 		this.find('[name]').each(function(){
-		    $(this).attr('name',$(this).attr('name').replace(/\[[0-9]*\]/g,'[]'));
+			var name = $(this).attr('name');
+			var findNum = new RegExp('\\[([0-9]+)\\]');
+			var foundNums = name.match(findNum);
+			if (!foundNums || !foundNums[1]){
+				return;
+			}
+			var num = 1*foundNums[1];
+			num++;
+			name = name.replace(findNum, '['+num+']');
+			$(this).attr('name', name);
 		});
 		return this;
 	};
